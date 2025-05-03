9e36143fb9923dbb0d0c216a4c25cdd97764bb0e
RAHUL A P <rahul.ap@aot-technologies.com>
2025-05-03 11:10:49 -0700

    mongo added

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv"
 
 import authRoutes from "./routes/auth.route.js"
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -13,4 +14,5 @@ app.use("/api/auth", authRoutes)
 
 app.listen(PORT,()=>{
     console.log(`Server is running on this port ${PORT}`)
+    connectDB();
 })
