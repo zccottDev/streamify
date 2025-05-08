a1c1ea8728eeaa5b6fa29b9e2fe98d4ac97ac8fc
RAHUL A P <rahul.ap@aot-technologies.com>
2025-05-08 05:58:37 -0500

    added user routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js"
-import { getMyFriends, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controllers.js"
+import { acceptFriendRequest, getFriendRequests, getMyFriends, getOutgoingFriendReqs, getRecommendedUsers, sendFriendRequest } from "../controllers/user.controllers.js"
 
 const router = express.Router()
 
@@ -9,9 +9,9 @@ router.use(protectRoute)
 router.get("/", getRecommendedUsers)
 router.get("/friends", getMyFriends)
 router.post("/friend-request/:id", sendFriendRequest);
-// router.put("/friend-request/:id/accept", acceptFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
-// router.get("/friend-requests", getFriendRequests);
-// router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
+router.get("/friend-requests", getFriendRequests);
+router.get("/outgoing-friend-requests", getOutgoingFriendReqs);
 
 export default router
